533c54947c218cdbbd2e6830a720d06a57d29e49
Sergei Bakhmatov <94bahmatov@gmail.com>
2023-06-10 11:59:02 +0500

    Add button

--- TrafficLightSwiftUI/ContentView.swift
+++ TrafficLightSwiftUI/ContentView.swift
@@ -10,9 +10,33 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            LightView(lightAlpha: 0.5, lightColor: .red)
+            LightView(lightAlpha: 0.5, lightColor: .yellow)
+            LightView(lightAlpha: 0.5, lightColor: .green)
+            
+            Spacer()
+            
+            ZStack {
+                Capsule()
+                    .foregroundColor(.gray)
+                    .frame(width: 150, height: 60)
+                    .overlay(Capsule().stroke(Color.black, lineWidth: 4))
+                Button(action: buttonPressed) {
+                    Text("START")
+                        .frame(width: 140, height: 50)
+                        .font(.title)
+                        .foregroundColor(.white)
+                        
+                }
+            }
         }
+        
         .padding()
     }
+}
+
+private func buttonPressed() {
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
